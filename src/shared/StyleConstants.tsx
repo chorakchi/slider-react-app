b7fcc29b5fae0644be79fb9292626eaa3cc01fe8
mochorak <mohammad.chorakchi@akelius.de>
2020-10-18 21:49:17 +0200

    feat(shared): implementation icon component

---
+++ src/shared/StyleConstants.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Icon: any = {
+  next: (
+    <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
+      />
+    </svg>
+  ),
+  back: (
+    <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
+      />
+    </svg>
+  ),
+};
---
+++ src/shared/index.tsx
@@ -0,0 +1,6 @@
+import { IconButton } from "./IconButton";
+import { Indicator } from "./Indicator";
+import { Slides } from "./Slides";
+import { Icon } from "./StyleConstants";
+
+export { IconButton, Indicator, Slides, Icon };
